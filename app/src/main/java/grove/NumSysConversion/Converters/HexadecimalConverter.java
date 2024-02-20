ddf1a4b585673f0498f5e811c4f9d3b9fe7d6fd3
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2024-02-20 12:02:54 -0400

    major: Hexadecimal to Decimal, Binary and Octal convertions

---
+++ app/src/main/java/grove/NumSysConversion/Converters/HexadecimalConverter.java
@@ -0,0 +1,7 @@
+package grove.NumSysConversion.Converters;
+
+public interface HexadecimalConverter {
+    long hexadecimalToDecimal(String hexadecimal);
+    String hexadecimalToBinary(String hexadecimal);
+    String hexadecimalToOctal(String hexadecimal);
+}
---
+++ app/src/main/java/grove/NumSysConversion/Hexadecimal.java
@@ -0,0 +1,49 @@
+package grove.NumSysConversion;
+
+import grove.NumSysConversion.Converters.HexadecimalConverter;
+
+public class Hexadecimal implements HexadecimalConverter {
+    private final Decimal decimal;
+
+    public Hexadecimal() {
+        decimal = new Decimal();
+    }
+
+    @Override
+    public long hexadecimalToDecimal(String hexadecimal) {
+        long decimal = 0;
+        int pow = 0;
+        for (int i = hexadecimal.length() - 1; i >= 0; i--) {
+            int powHex = (int) Math.pow(16, pow);
+            if (!Character.isDigit(hexadecimal.charAt(i))) {
+                decimal += getIndexLetter(hexadecimal.charAt(i)) * powHex;
+            } else {
+                decimal += (long) Character.getNumericValue(hexadecimal.charAt(i)) * powHex;
+            }
+            pow++;
+        }
+        return decimal;
+    }
+
+    @Override
+    public String hexadecimalToBinary(String hexadecimal) {
+        long decimal = hexadecimalToDecimal(hexadecimal);
+        return this.decimal.decimalToBinary((int) decimal);
+    }
+
+    @Override
+    public String hexadecimalToOctal(String hexadecimal) {
+        long decimal = hexadecimalToDecimal(hexadecimal);
+        return this.decimal.decimalToOctal(decimal);
+    }
+
+    private long getIndexLetter(char letter) {
+        long count = 10;
+        char initChar = 'A';
+        while (Character.toUpperCase(initChar) != letter) {
+            count++;
+            initChar++;
+        }
+        return count;
+    }
+}
