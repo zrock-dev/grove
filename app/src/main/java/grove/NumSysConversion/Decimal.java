f896a9e3e208ab5f459303552768a2d697be665f
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2024-02-20 11:46:34 -0400

    major: decimal to binary, octal and hexa operations added

---
+++ app/src/main/java/grove/NumSysConversion/Decimal.java
@@ -0,0 +1,54 @@
+package grove.NumSysConversion;
+
+public class Decimal implements DecimalConverter {
+    @Override
+    public String decimalToBinary(long decimal) {
+        StringBuilder stringBuilder;
+        if (decimal > 0) {
+            String binary = "";
+            while (decimal != 0) {
+                binary += decimal % 2;
+                decimal /= 2;
+            }
+            stringBuilder = new StringBuilder(binary);
+            return stringBuilder.reverse().toString();
+        }
+        return null;
+    }
+
+    @Override
+    public String decimalToOctal(long decimal) {
+        StringBuilder stringBuilder;
+        if (decimal > 8) {
+            String octal = "";
+            while (decimal > 8) {
+                octal += decimal % 8;
+                decimal /= 8;
+            }
+            octal += decimal;
+            stringBuilder = new StringBuilder(octal);
+            return stringBuilder.reverse().toString();
+        } else {
+            return Long.toString(decimal);
+        }
+    }
+
+    @Override
+    public String decimalToHexadecimal(long decimal) {
+        StringBuilder stringBuilder;
+        String hexadecimalNumber = "0123456789ABCDEF";
+        String hexadecimal = "";
+        while (decimal > 0) {
+            int module = (int) (decimal % 16);
+            hexadecimal += hexadecimalNumber.charAt(module);
+            decimal /= 16;
+        }
+        stringBuilder = new StringBuilder(hexadecimal);
+        return stringBuilder.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Decimal decimal = new Decimal();
+        System.out.println(decimal.decimalToHexadecimal(1232131));
+    }
+}
---
+++ app/src/main/java/grove/NumSysConversion/DecimalConverter.java
@@ -0,0 +1,7 @@
+package grove.NumSysConversion;
+
+public interface DecimalConverter {
+    String decimalToBinary(long decimal);
+    String decimalToOctal(long decimal);
+    String decimalToHexadecimal(long decimal);
+}
